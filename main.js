f02e029fc7d60c80e53e92530bc0e1ceb81e1d2e
Fathma Khatun Mim <fathmakhatunmim@gmail.com>
2025-10-27 12:54:51 +0600

    responsive

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-  const gallery = document.getElementById("gallery");
-  const leftBtn = document.getElementById("leftBtn");
-  const rightBtn = document.getElementById("rightBtn");
 
-  rightBtn.addEventListener("click", () => {
-    gallery.scrollBy({ left: 300, behavior: "smooth" });
-  });
+  
 
-  leftBtn.addEventListener("click", () => {
-    gallery.scrollBy({ left: -300, behavior: "smooth" });
-  });
+ 
